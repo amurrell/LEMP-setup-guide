4d1ec107914488bb9916df960c5ef5d3d1ed6415
Angela Murrell <amasiell.g@gmail.com>
2018-07-24 03:45:43 -0700

    make sure certbot only is installed

--- scripts/install-cert.sh
+++ scripts/install-cert.sh
@@ -4,7 +4,7 @@ if ! [ -x "$(command -v certbot)" ]; then
   sudo apt-get install -y software-properties-common
   LC_ALL=C.UTF-8 sudo add-apt-repository -y ppa:certbot/certbot
   sudo apt-get update
-  sudo apt-get install -y python-certbot-nginx 
+  sudo apt-get install -y certbot 
 fi
 
 # check registered
